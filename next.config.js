c101ddffc4a22e5e93117ff465cc378c092d0edb
Districorr <sistemas@districorr.com.ar>
2025-04-23 22:13:17 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,18 @@
-const path = require('path');
-
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: [],
+  },
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
       '@': path.resolve(__dirname),
-      '@lib': path.resolve(__dirname, './lib')
+      '@lib': path.resolve(__dirname, 'lib'),
+      '@components': path.resolve(__dirname, 'components'),
     };
     return config;
-  }
+  },
 };
+
+module.exports = nextConfig;
