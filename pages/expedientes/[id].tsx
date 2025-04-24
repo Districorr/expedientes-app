babf55dd97865a4cfc5e30833eabd4efaddc7408
Districorr <sistemas@districorr.com.ar>
2025-04-23 23:09:49 -0300

    Update [id].tsx

--- pages/expedientes/[id].tsx
+++ pages/expedientes/[id].tsx
@@ -7,18 +7,18 @@ import { Expediente } from '../../types/expediente';
 const ExpedientePage = () => {
   const router = useRouter();
   const { id } = router.query;
+
   const [expediente, setExpediente] = useState<Expediente | null>(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    if (id) {
-      getExpedienteById(id as string)
-        .then((data) => {
-          if (data) {
-            setExpediente(data as Expediente);
-          }
-        })
-        .finally(() => setLoading(false));
+    if (typeof id === 'string') {
+      getExpedienteById(id).then((data) => {
+        if (data && data.numero && data.estado && data.usuario && data.fechaIngreso) {
+          setExpediente(data as Expediente);
+        }
+        setLoading(false);
+      });
     }
   }, [id]);
 
@@ -27,7 +27,7 @@ const ExpedientePage = () => {
   }
 
   return (
-    <NewExpedienteForm initialData={{ id: id as string, ...expediente }} />
+    <NewExpedienteForm initialData={{ ...expediente, id: id as string }} />
   );
 };
 
